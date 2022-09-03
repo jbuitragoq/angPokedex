509d62d9c62bb11e99680b3bd1bd75831fceaf77
DavidTorres <dadyotorres1403@gmail.com>
2022-09-03 00:42:32 -0500

    appnamr

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/CIFRADOAFIN'));
+app.use(express.static('./dist/cifrado-afin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/CIFRADOAFIN/src'}),
+    res.sendFile('index.html', {root: 'dist/cifrado-afin/'}),
 );
 
 // Start the app by listening on the default Heroku port
