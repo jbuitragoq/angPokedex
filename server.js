71a3912868dc1418639f7797c574c4afe15fded3
DavidTorres <dadyotorres1403@gmail.com>
2022-09-29 12:46:32 -0500

    set new server settings

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const path = require('path');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
+
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/cifrado-afin'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/cifrado-afin/'}),
+    res.sendFile('index.html', { root: 'dist/cifrado-afin/' }),
 );
 
 // Start the app by listening on the default Heroku port
