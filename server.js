e9a7055122c347c0171cf705e0d16acb3bac6fb9
jbuitragoq <johnf.buitragoq@gmail.com>
2022-10-04 12:12:56 -0500

    config

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require('path');
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use((req, res, next) => {
-//         if (req.header('x-forwarded-proto') !== 'https')
-//             res.redirect(`https://${req.header('host')}${req.url}`)
-//         else
-//             next()
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/pokedex-app'));
